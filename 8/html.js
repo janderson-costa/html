a01bab81ea9fbd51b81e1a714308e5ddcd58cfc4
janderson-costa <janderson.dev@outlook.com>
2025-05-02 14:21:30 -0300

    ---

--- 8/html.js
+++ 8/html.js
@@ -15,6 +15,15 @@ function html(templateString, ...expressions) {
 
 	return _component;
 
+	function reload() {
+		// Recria um novo componente e substitui o anterior.
+
+		const newComponent = createComponent();
+
+		_component.replaceWith(newComponent);
+		_component = newComponent;
+	}
+
 	function createComponent() {
 		const html = parseTemplateString();
 		const component = createElement(html);
@@ -29,27 +38,38 @@ function html(templateString, ...expressions) {
 
 	function parseTemplateString() {
 		const htmlParts = _templateString;
-		const html = htmlParts.reduce((acc, cur, index) => {
-			const part = compressTemplateString(htmlParts[index - 1]);
-			const onEventRegex = /@on[a-zA-Z0-9]*="$/; // Termina com @on<eventName>="
-
+		const html = htmlParts.reduce((acc, cur, i) => {
 			acc = compressTemplateString(acc);
 			cur = compressTemplateString(cur);
 
-			if (index == 0)
+			if (i == 0)
 				return cur;
 
-			let expression = _expressions[index - 1];
+			const index = i - 1;
+			const part = compressTemplateString(htmlParts[i - 1]);
+			const onEventRegex = /@on[a-zA-Z0-9]*="$/; // Termina com @on<eventName>="
+
+			let expression = _expressions[index];
 			let isFunction = typeof expression == 'function';
 
 			if (isElement(expression)) {
-				>>>>>>>>>>>
+				expression = `<element>${index}</element>`;
 			} else if (isFunction && (part.endsWith('>') || !onEventRegex.test(part))) {
 				isFunction = false;
 				expression = expression();
+
+				if (isElement(expression)) {
+					expression = `<function>${index}</function>`;
+				}
 			}
 
-			let html = acc + (isFunction ? index - 1 : expression) + cur;
+			let html = acc + (isFunction ? index : expression) + cur;
+
+			html = html
+				.replaceAll('selected="true"', 'selected')
+				.replaceAll('selected="false"', '')
+				.replaceAll('checked="true"', 'checked')
+				.replaceAll('checked="false"', '');
 
 			return html;
 		}, '');
@@ -72,17 +92,22 @@ function html(templateString, ...expressions) {
 		return doc.body.firstChild;
 	}
 
-	function reload() {
-		// Recria um novo componente e substitui o anterior.
-
-		const newComponent = createComponent();
-
-		_component.replaceWith(newComponent);
-		_component = newComponent;
-	}
-
 	function setComponent(element) {
 		// Configura o componente e todos os seus elementos.
+
+		const elements = element.querySelectorAll('element, function');
+
+		elements.forEach(element => {
+			const expression = element.tagName == 'FUNCTION' ? _expressions[element.textContent]() : _expressions[element.textContent];
+			const children = expression instanceof Array ? expression : [expression];
+
+			children.forEach((child, index) => {
+				element.before(child);
+
+				if (index == children.length - 1)
+					element.remove();
+			});
+		});
 
 		set(element);
 
@@ -93,63 +118,22 @@ function html(templateString, ...expressions) {
 				setComponent(child);
 		});
 
-		function set(child) {
-			Array.from(child.attributes).forEach(attr => {
+		function set(element) {
+			Array.from(element.attributes).forEach(attr => {
 				const attrName = attr.name.toLowerCase();
 
 				// @onEvent
 				if (attrName.startsWith('@on')) {
 					const func = _expressions[attr.value];
-					const _attrName = attrName.substring(3); // @onclick => click, @onchange => change, ...
+					const _attrName = attrName.substring(3);
 
-					child.addEventListener(_attrName, event =>
+					element.addEventListener(_attrName, event =>
 						func({ event, element: event.target, reload })
 					);
 
-					child.removeAttribute(attrName);
+					element.removeAttribute(attrName);
 				}
 			});
 		}
-	}
-}
-
-function ___parseTemplateString() {
-	const htmlParts = _templateString;
-	const html = htmlParts.reduce((acc, cur, index) => {
-		const part = compressTemplateString(htmlParts[index - 1]);
-		const onEventRegex = /@on[a-zA-Z0-9]*="$/; // Termina com @on<eventName>="
-
-		acc = compressTemplateString(acc);
-		cur = compressTemplateString(cur);
-
-		if (index == 0)
-			return cur;
-
-		let expression = _expressions[index - 1];
-		let isFunction = typeof expression == 'function';
-
-		if (isFunction && (part.endsWith('>') || !onEventRegex.test(part))) {
-			isFunction = false;
-			expression = expression();
-
-			if (isElement(expression)) {
-				console.log(expression);
-				isFunction = true;
-			}
-		}
-
-		const html = acc + (isFunction ? index - 1 : expression) + cur;
-
-		return html;
-	}, '');
-
-	return html;
-
-	function isElement(any) {
-		return any instanceof HTMLElement || any[0] instanceof HTMLElement;
-	}
-
-	function compressTemplateString(text) {
-		return typeof text == 'string' ? text.replace(/\n|\t/g, '').trim() : '';
 	}
 }
--- 8/index.js
+++ 8/index.js
@@ -1,20 +1,27 @@
 import { html } from './html.js';
 
-const origens = ['Correio', 'E-Mail', 'Fone'];
+const origins = ['Correio', 'E-Mail', 'Fone'];
+const colors = ['Red', 'Blue', 'Green'];
 const items = [
 	{ name: 'Correio' },
 	{ name: 'E-Mail' },
 	{ name: 'Fone' },
 ];
 const props = {
-	Componente1: { name: 'Componente 1', items: items, color: 'Red', origemRadio: 'E-Mail', origemCheckbox: ['Correio', 'Fone']},
-	Componente2: { name: 'Componente 2', items: items },
+	Componente1: { name: 'Componente 1', color: 'Blue', origemRadio: 'E-Mail', origem: ['Correio', 'Fone']},
+	Componente2: { name: 'Componente 2' },
 };
+const select = html`
+	<select @onChange="${e => props.Componente1.color = e.element.value}">${() => colors.map((color, index) =>
+		html`<option type="checkbox" selected="${props.Componente1.color == color}">${color}</option>`
+	)}</select>
+`;
 
 // ! Exemplo 1
 const comp1 = html`
 	<div style="display: inline-flex; flex-direction: column; gap: 8px;">
 		<h1>${() => props.Componente1.name}</h1>
+
 		<div>
 			<button @onClick="${e => console.log(e.event)}">Test</button>
 			<button @onClick="${e => {
@@ -22,38 +29,52 @@ const comp1 = html`
 				e.reload();
 			}}">Change</button>
 			<button @onClick="${e => {
-				props.Componente1.items.push({ name: new Date().toUTCString() });
+				items.push({ name: new Date().toUTCString() });
 				e.reload();
 			}}">Add</button>
 			<button @onClick="${e => {
-				props.Componente1.items.pop(1);
+				items.pop(1);
 				e.reload();
 			}}">Remove</button>
 		</div>
+
 		<input type="text" value="${() => props.Componente1.name}" @onChange="${e => {
 			props.Componente1.name = e.element.value;
 			e.reload();
 		}}" />
+
 		<textarea @onChange="${e => {
 			props.Componente1.color = e.element.value;
 			e.reload();
 		}}">${() => props.Componente1.color}</textarea>
-		${props.Componente1.items.map((item, index) => html`
+
+		${select}
+		${select.cloneNode(true)}
+
+		${() => items.map((item, index) => html`
 			<label>
-				<input type="checkbox" name="checkboxOrigem" @onChange="${e => {
-					item.checked = e.element.checked;
+				<input type="checkbox" name="checkboxOrigem" checked="${props.Componente1.origem.some(x => x == item.name)}" @onChange="${e => {
+					console.log(e.element.checked);
 				}}">${item.name}
-			</label>`
-		)}
+			</label>
+		`)}
+
+		${() => origins.map((item, index) => html`
+			<label>
+				<input type="radio" name="radioOrigem" @onChange="${e => {
+					//..
+				}}">${item}
+			</label>
+		`)}
 	</div>
 `;
 
 document.body.appendChild(comp1);
 
-// ${() => props.Componente1.items.map((item, index) =>
+// ${() => items.map((item, index) =>
 // 	html`<li @onClick="${e => console.log(e.element)}">${ index + ' - ' + item.name}</li>`.outerHTML
 // ).join('')}
 
-// {/* <label @for="${props.Componente1.items}">
+// {/* <label @for="${items}">
 // 	<input type="checkbox" name="checkboxOrigem" @onChange="${e => e.item.checked = e.element.checked}">{item.name}
 // </label> */}
